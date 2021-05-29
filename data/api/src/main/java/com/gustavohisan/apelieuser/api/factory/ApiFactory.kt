6c8ae7f8cf49467a031a6c9d85d043591942afb8
Shockwaiver <38588489+Shockwaiver@users.noreply.github.com>
2021-05-29 02:33:01 -0300

    PS-150 - Create network interceptor
    Created a network interceptor used to handle network issues and prevent the application from crashing.

--- data/api/src/main/java/com/gustavohisan/apelieuser/api/factory/ApiFactory.kt
+++ data/api/src/main/java/com/gustavohisan/apelieuser/api/factory/ApiFactory.kt
@@ -1,16 +1,21 @@
 package com.gustavohisan.apelieuser.api.factory
 
 import com.google.gson.GsonBuilder
+import com.gustavohisan.apelieuser.api.interceptor.NetworkInterceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 /**
  * Factory used to retrieve the API instance available.
+ *
+ * @param networkInterceptor interceptor used to handle network issues
  */
-internal class ApiFactory {
+internal class ApiFactory(private val networkInterceptor: NetworkInterceptor) {
 
     private val retrofit = Retrofit.Builder()
         .baseUrl(API_URL)
+        .client(getOkHttpClient())
         .addConverterFactory(GsonConverterFactory.create(GsonBuilder().setLenient().create()))
         .build()
 
@@ -20,6 +25,12 @@ internal class ApiFactory {
      * @return a [Retrofit] built with the API url
      */
     fun getRetrofitInstance(): Retrofit = retrofit
+
+    private fun getOkHttpClient(): OkHttpClient =
+        OkHttpClient()
+            .newBuilder()
+            .addInterceptor(networkInterceptor)
+            .build()
 
     private companion object {
 
--- data/api/src/main/java/com/gustavohisan/apelieuser/api/injection/ApiModule.kt
+++ data/api/src/main/java/com/gustavohisan/apelieuser/api/injection/ApiModule.kt
@@ -9,6 +9,7 @@ import com.gustavohisan.apelieuser.api.mapper.register.RegisterStateMapper
 import com.gustavohisan.apelieuser.api.mapper.store.StoreMapper
 import com.gustavohisan.apelieuser.api.mapper.store.StoreStateMapper
 import com.gustavohisan.apelieuser.api.factory.ApiFactory
+import com.gustavohisan.apelieuser.api.interceptor.NetworkInterceptor
 import com.gustavohisan.apelieuser.repository.datasource.login.UserApiDataSource
 import com.gustavohisan.apelieuser.repository.datasource.store.StoreDataSource
 import org.koin.dsl.module
@@ -31,5 +32,8 @@ val apiModule = module {
     factory { StoreMapper() }
 
     // Provider
-    single { ApiFactory() }
+    single { ApiFactory(get()) }
+
+    // Interceptors
+    factory { NetworkInterceptor() }
 }
